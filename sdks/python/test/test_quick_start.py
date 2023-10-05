f8a54fab6f9ae0f434993270e85dccadaa628987
dphuang2 <dylan.p.huang@gmail.com>
2023-10-05 01:01:51 -0700

    add test_quick_start_local in Python SDK

--- sdks/python/test/test_quick_start.py
+++ sdks/python/test/test_quick_start.py
@@ -52,6 +52,29 @@ class TestSimple(unittest.TestCase):
         search = groundx.search.content(project_id=project["project"]["projectId"], search={"query": "Documents"})
         pprint(search.body)
 
+    def test_quick_start_local(self):
+        groundx = Groundx(
+            api_key = os.environ['GROUNDX_API_KEY'],
+        )
+
+        project = groundx.project.create(project={"name": "test-project"}).body
+        bucket = groundx.bucket.create(bucket={"name": "test-bucket"}).body["bucket"]
+        # create file instance with open() for ../../../document.txt. Use path relative to this file
+        file = open(os.path.join(os.path.dirname(__file__), "../../../document.txt"), "rb")
+        ingest = groundx.document.upload_local(blob=[file], metadata={"bucketId": bucket["bucketId"], "fileName": "document.txt", "fileType": "txt"}).body
+
+        # poll the status of upload until it is complete
+        while ingest["ingest"]["status"] != "complete":
+            ingest = groundx.document.get_processing_status_by_id(ingest["ingest"]["processId"]).body
+
+            # sleep for 3 seconds
+            import time
+            time.sleep(3)
+
+        # search
+        search = groundx.search.content(project_id=project["project"]["projectId"], search={"query": "Documents"})
+        pprint(search.body)
+
 
     def tearDown(self):
         pass
