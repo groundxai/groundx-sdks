5c6aaf9c987fc1f8a89fb6cb8a1a75fe99cb00b9
konfig-publisher <publisher@konfigthis.com>
2024-07-25 20:14:35 +0000

    Version Bump SDKs

--- sdks/python/groundx/__init__.py
+++ sdks/python/groundx/__init__.py
@@ -12,7 +12,7 @@
     Created by: https://www.groundx.ai/
 """
 
-__version__ = "1.3.20"
+__version__ = "1.3.21"
 
 # import ApiClient
 from groundx.api_client import ApiClient
--- sdks/python/groundx/configuration.py
+++ sdks/python/groundx/configuration.py
@@ -408,7 +408,7 @@ conf = groundx.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 1.3.20".\
+               "SDK Package Version: 1.3.21".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
